caa6cda73d4d4b7b025703b545d24fe04a8b3298
mohankumar.sf07@gmail.com <mohankumar.sf07@gmail.com>
2026-03-11 14:32:30 +0000

    updated

---
+++ models/example/stage/BASED_ON_JOB_ID_SALARY.sql
@@ -0,0 +1,19 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+
+{% set job_ids=['LG','HRM','SEC','TR','CS'] %}
+
+
+
+
+select job_id,
+ {% for j_id in job_ids %}
+sum(case when job_id = '{{j_id}}' then salary end) as {{j_id}}_tot_amount,
+{% if not loop.last %},{% endif %}
+{% endfor %}
+from employees
+group by 1
