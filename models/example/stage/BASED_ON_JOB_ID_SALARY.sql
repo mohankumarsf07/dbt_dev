aae1523b19ef9dac0ae16109f81bf6f601cd96c0
mohankumar.sf07@gmail.com <mohankumar.sf07@gmail.com>
2026-03-12 04:55:02 +0000

    MADE CHAINGES TO LOWER CASE TO UPPER IN VARABLE LIST

--- models/example/stage/BASED_ON_JOB_ID_SALARY.sql
+++ models/example/stage/BASED_ON_JOB_ID_SALARY.sql
@@ -5,7 +5,7 @@
 }}
 
 
-{% set job_ids= ["LG","HRM","SEC","TR","CS"] %}
+{% set job_ids= ["LG","HRM","CS","sa",'sec','pm','op'] %}
 
 
 
@@ -13,7 +13,7 @@
 select job_id,
 
 {% for j_id in job_ids %}
- sum(case when job_id = '{{j_id}}' then salary end) as {{j_id}}_tot_amount,
+ sum(case when job_id = '{{j_id}}' then salary end) as {{j_id}}_tot_amount
  {% if not loop.last %},{% endif %}
  {% endfor %}
- from employees group by 1
+ from {{ ref('RAW_EMPLOYEES') }} group by 1
