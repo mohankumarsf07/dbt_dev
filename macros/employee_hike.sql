0a818ffc552ac0fbe57c1f0afe335c594530f0ae
mohankumar.sf07@gmail.com <mohankumar.sf07@gmail.com>
2026-03-14 10:51:27 +0000

    applyed macro in model updated for 30 percentage hike in all employee

--- macros/employee_hike.sql
+++ macros/employee_hike.sql
@@ -1,7 +1,7 @@
 {% macro employee_hike(hike_per) %}
 
 
-(SALARY+(SALARY*("hike_per"/100)))
+(SALARY+(SALARY*({{hike_per}}/100)))
 
 
 
--- models/example/stage/employee_hike_percentage.sql
+++ models/example/stage/employee_hike_percentage.sql
@@ -5,6 +5,5 @@
 }}
 
 
--- select employee_id,concat(first_name,'  ',last_name) as full_name,salary,{{ employee_hike() }} as hike_sal from {{ ref('RAW_EMPLOYEES') }}
+select employee_id,concat(first_name,'  ',last_name) as full_name,salary,{{ employee_hike('30') }} as Sal_hike from {{ ref('RAW_EMPLOYEES') }}
 
-select employee_id,concat(first_name,'  ',last_name) as full_name,salary,{{ employee_hike('30') }} as hike_sal from {{ ref('RAW_EMPLOYEES') }}
