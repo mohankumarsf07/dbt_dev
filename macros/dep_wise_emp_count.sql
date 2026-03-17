ff9ea1c13c0e6fa378a58da01dfb6e93866798ce
mohankumar.sf07@gmail.com <mohankumar.sf07@gmail.com>
2026-03-17 07:43:56 +0000

    adde macros

--- macros/dep_wise_emp_count.sql
+++ macros/dep_wise_emp_count.sql
@@ -2,11 +2,8 @@
 
 
 select {{dept_tbl}}.department_name,count({{emp_tbl}}.employee_id) as num_of_employees
-from {{ dept_tbl }} inner join {{emp_tble}}
-on {{dept_tbl}}.department_id={{emp_tbl}}.department_id
+from {{ dept_tbl }} inner join {{ emp_tbl }}
+on {{ dept_tbl }}.department_id={{ emp_tbl }}.department_id
 group by {{dept_tbl}}.department_name
-
-
-
 
 {% endmacro %}
---
+++ models/example/stage/num_of_emp_in_each_dept.sql
@@ -0,0 +1,8 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+
+{{ dept_wise_emp_count('raw_departments'),('raw_employees') }}
