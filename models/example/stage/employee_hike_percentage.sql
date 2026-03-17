6f8804c5ef4de41c9789916cc1eac01c2b5c4ea1
mohankumar.sf07@gmail.com <mohankumar.sf07@gmail.com>
2026-03-17 10:54:16 +0000

    removve comments in ffile

--- models/example/stage/employee_hike_percentage.sql
+++ models/example/stage/employee_hike_percentage.sql
@@ -10,9 +10,5 @@ from {{ ref('RAW_EMPLOYEES') }}
 
 
 
-SELECT
-    {{ dbt_utils.generate_surrogate_key(['employee_id','department_id']) }} AS emp_key,
-    employee_id,
-    department_id
-FROM {{ ref('RAW_EMPLOYEES') }}
+
 
