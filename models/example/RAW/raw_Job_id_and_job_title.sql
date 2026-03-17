4107f3fd282e66c73dbcdfba89c2efe5dcc195e1
mohankumar.sf07@gmail.com <mohankumar.sf07@gmail.com>
2026-03-17 10:54:54 +0000

    removve comments in the files

--- models/example/RAW/raw_Job_id_and_job_title.sql
+++ models/example/RAW/raw_Job_id_and_job_title.sql
@@ -1,11 +1,5 @@
 
 
---direrctly we can call seed ffile becouse its in snowfflake
-
--- -- select * from {{ ref('country_codes') }}
-
--- -- select * from {{ ref('Job_id_and_job_title') }}
-
 {{
     config(
         materialized='view'
